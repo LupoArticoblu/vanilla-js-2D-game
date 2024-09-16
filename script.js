2897e7954f05906e387bd78bbab88271b9f1f355
LupoArticoblu <wolf94-@libero.it>
2024-09-16 13:12:58 +0200

    input keys

--- script.js
+++ script.js
@@ -9,9 +9,24 @@ window.addEventListener("load", function(){
     constructor() {
       this.keys=[];
       window.addEventListener("keydown", e => {
-        
-      })
-
+        //se l'evento ha un pulsante premuto(freccia giù), aggiungilo alla lista dei pulsanti premuti solo se non vi è gia'
+        if((e.key === "ArrowDown"|| 
+          e.key === "ArrowUp" || 
+          e.key === "ArrowLeft" || 
+          e.key === "ArrowRight") 
+          && this.keys.indexOf(e.key) === -1) {
+            this.keys.push(e.key);
+        }
+      });
+      window.addEventListener('keyup', e => {
+        if(e.key === "ArrowDown"
+          || e.key === "ArrowUp"
+          || e.key === "ArrowLeft"
+          || e.key === "ArrowRight"
+        ) {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      })  
     }
   }
 
