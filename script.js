d78a31dfcb2db42cbf418c7fa2314fbd6c0f96c0
LupoArticoblu <wolf94-@libero.it>
2024-09-16 12:56:20 +0200

    markup iniziale e struttura script

---
+++ script.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", function(){
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 600;
+
+  //terrà conto dei pulsanti premuti
+  class InputHandler {
+    constructor() {
+      this.keys=[];
+      window.addEventListener("keydown", e => {
+        
+      })
+
+    }
+  }
+
+  class Player {
+
+  }
+
+  class Background {
+
+  }
+
+  class Enemy {
+
+  }
+
+  function handleEnemy(){
+
+  }
+
+  function displayStatusText(){
+
+  }
+
+  function animate(){
+
+  }
+});
