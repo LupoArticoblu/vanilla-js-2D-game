1085c2b66d47a59ac1bd2c12f8def36db669d0cf
LupoArticoblu <wolf94-@libero.it>
2024-09-16 15:32:24 +0200

    primo movimento condizionato

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ window.addEventListener("load", function(){
             this.keys.push(e.key);
         }
       });
+      //rimuovi i pulsanti premuti una volta che il pulsante viene rilasciato
       window.addEventListener('keyup', e => {
         if(e.key === "ArrowDown"
           || e.key === "ArrowUp"
@@ -31,7 +32,32 @@ window.addEventListener("load", function(){
   }
 
   class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameHeight = gameHeight;
+      this.gameWidth = gameWidth;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById('player');
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
+    }
 
+    draw(context) {
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    update(input) {
+      this.x += this.speed; //<-movimento orizzontale
+      if(input.keys.indexOf('ArrowRight') > -1) { 
+        this.speed = 5;
+      }else{
+        this.speed = 0;
+      }
+    }
   }
 
   class Background {
@@ -50,7 +76,17 @@ window.addEventListener("load", function(){
 
   }
 
-  function animate(){
+  const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  const background = new Background();
+ 
 
+  function animate(){
+    player.draw(ctx);
+    player.update(input);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    requestAnimationFrame(animate);
   }
+
+  animate();
 });
